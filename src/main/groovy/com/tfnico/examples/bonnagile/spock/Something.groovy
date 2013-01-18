a06796a95b08d9369a2ffb929e3f4b46604efe43
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2013-01-18 13:54:03 +0100

    Maven Java project with Groovy tests and Spock

---
+++ src/main/groovy/com/tfnico/examples/bonnagile/spock/Something.groovy
@@ -0,0 +1,10 @@
+package com.tfnico.examples.bonnagile.spock
+/**
+ * Created with IntelliJ IDEA.
+ * User: tfnico
+ * Date: 1/18/13
+ * Time: 12:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Something {
+}
---
+++ src/main/java/com/tfnico/examples/bonnagile/spock/App.java
@@ -0,0 +1,21 @@
+package com.tfnico.examples.bonnagile.spock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void doTheFoo() {
+        didIt();
+    }
+
+    public void didIt() {
+        //done it
+    }
+}
